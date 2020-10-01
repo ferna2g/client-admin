a1b3b03272e46587110ea809c918d54208fe3a56
fer g <arielgeronimo517@gmail.com>
2020-09-30 20:38:16 -0600

    creando el formulario para nuevos proyectos

---
+++ src/components/proyectos/Proyectos.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+import Sidebar from '../layout/Sidebar'
+
+const Proyectos = () => {
+  return (
+    <div className="contendor-app">
+
+      <div className="seccion-principal">
+      <Sidebar />
+
+        <main>
+          <div className="contenedor-tareas">
+
+          </div>
+        </main>
+      </div>
+    </div>
+  )
+}
+
+export default Proyectos
