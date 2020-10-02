ab164d8c738fedb3594d1757209cf1d02529d439
fer g <arielgeronimo517@gmail.com>
2020-10-02 17:54:09 -0600

    probando la interfaz de tareas

--- src/components/tareas/Tarea.jsx
+++ src/components/tareas/Tarea.jsx
@@ -1,10 +1,31 @@
 import React from 'react'
 
-const Tarea = () => {
+const Tarea = ({tarea}) => {
 
   return (
-    <h2>desde tarea</h2>
+    <li className="tarea sombra">
+      <p>{tarea.nombre}</p>
+
+      <div className="estado">
+        {tarea.estado
+          ?
+            (
+              <button
+                  type="button"
+                  className="completo"
+                >Completo</button>
+            )
+          :
+            (
+              <button
+                  type="button"
+                  className="incompleto"
+                >Incompleto</button>
+            )
+        }
+      </div>
+    </li>
   )
 }
 
-export default Tarea 
+export default Tarea
