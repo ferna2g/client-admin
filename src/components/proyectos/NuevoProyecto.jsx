cd3a23efce258a22c699afc8b67eb2f38e5e17f6
fer g <arielgeronimo517@gmail.com>
2020-10-01 20:03:17 -0600

    state y funciones para nuevos proyectos

--- src/components/proyectos/NuevoProyecto.jsx
+++ src/components/proyectos/NuevoProyecto.jsx
@@ -1,27 +1,51 @@
-import React, { Fragment } from 'react'
+import React, { Fragment, useState } from 'react'
 
 const NuevoProyecto = () => {
 
+  //State para proyecto
+  const [ proyecto, guardarProyecto ] = useState({
+    nombre:''
+  })
+
+  //extraer nombre del proyecto
+  const { nombre } = proyecto
+
+  //Lee los contenidos del input
+  const onChangeProyecto = e => {
+    guardarProyecto({
+      ...proyecto,
+      [e.target.name] : e.target.value
+    })
+  }
+
+  //Cuando el usuario envia un proyecto
+  const onSubmitProyecto = e => {
+    e.preventDefault()
+  }
+
   return (
     <Fragment>
-    <button
-        type="button"
-        className="btn btn-block btn-primario"
-    >Nuevo Proyecto</button>
+        <button
+            type="button"
+            className="btn btn-block btn-primario"
+        >Nuevo Proyecto</button>
 
-    <form className="formulario-nuevo-proyecto" >
-      <input
-          type="text"
-          className="input-text"
-          placeholder="Nombre Proyecto"
-          name="nombre"
-       />
-       <input
-          type="submit"
-          className="btn btn-block btn-primario"
-          value="Agregar Proyecto"
-          />
-    </form>
+        <form className="formulario-nuevo-proyecto"
+              onSubmit={onChangeProyecto}>
+          <input
+              type="text"
+              className="input-text"
+              placeholder="Nombre Proyecto"
+              name="nombre"
+              value={nombre}
+              onChange={onChangeProyecto}
+           />
+           <input
+              type="submit"
+              className="btn btn-block btn-primario"
+              value="Agregar Proyecto"
+              />
+        </form>
   </Fragment>
   )
 }
