a2b4c4a3ad1f07f8e9230b4a6355174378abf2ca
fer g <arielgeronimo517@gmail.com>
2020-10-04 10:57:06 -0600

    mostrando el formulario de nuevos proyectos de forma condicional

--- src/components/proyectos/NuevoProyecto.jsx
+++ src/components/proyectos/NuevoProyecto.jsx
@@ -1,6 +1,11 @@
-import React, { Fragment, useState } from 'react'
+import React, { Fragment, useState, useContext } from 'react'
+import proyectoContext from '../../context/proyectos/proyectoContext'
 
 const NuevoProyecto = () => {
+
+  //obtener el state del formulario
+  const proyectosContext = useContext(proyectoContext)
+  const { formulario } = proyectosContext
 
   //State para proyecto
   const [ proyecto, guardarProyecto ] = useState({
@@ -30,22 +35,30 @@ const NuevoProyecto = () => {
             className="btn btn-block btn-primario"
         >Nuevo Proyecto</button>
 
-        <form className="formulario-nuevo-proyecto"
-              onSubmit={onChangeProyecto}>
-          <input
-              type="text"
-              className="input-text"
-              placeholder="Nombre Proyecto"
-              name="nombre"
-              value={nombre}
-              onChange={onChangeProyecto}
-           />
-           <input
-              type="submit"
-              className="btn btn-block btn-primario"
-              value="Agregar Proyecto"
-              />
-        </form>
+        {
+          formulario
+          ?
+          (
+          <form className="formulario-nuevo-proyecto"
+                onSubmit={onChangeProyecto}>
+            <input
+                type="text"
+                className="input-text"
+                placeholder="Nombre Proyecto"
+                name="nombre"
+                value={nombre}
+                onChange={onChangeProyecto}
+             />
+             <input
+                type="submit"
+                className="btn btn-block btn-primario"
+                value="Agregar Proyecto"
+                />
+          </form>
+        )
+          :
+          null
+        }
   </Fragment>
   )
 }
