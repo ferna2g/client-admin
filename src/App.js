f5097efff0d21618483211a09e6a04e5cf43206c
fer g <arielgeronimo517@gmail.com>
2021-01-18 21:42:12 -0600

    agregando state y provider para las tareas

--- src/App.js
+++ src/App.js
@@ -5,19 +5,22 @@ import Proyectos from './components/proyectos/Proyectos'
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
 
 import ProyectoState from './context/proyectos/proyectoState'
+import TareaState from './context/tareas/tareaState'
 
 function App() {
   //<Route exact path="/nueva-cuenta" component={NuevaCuenta} />
   //<Route exact path="/proyectos" component={Proyectos} />
   return (
     <ProyectoState>
-    <Router>
-      <Switch>
-          <Route exact path="/" component={Login} />
-          <Route exact path="/nueva-cuenta" component={NuevaCuenta} />
-          <Route exact path="/proyectos" component={Proyectos} />
-      </Switch>
-    </Router>
+      <TareaState>
+        <Router>
+          <Switch>
+              <Route exact path="/" component={Login} />
+              <Route exact path="/nueva-cuenta" component={NuevaCuenta} />
+              <Route exact path="/proyectos" component={Proyectos} />
+          </Switch>
+        </Router>
+      </TareaState>
     </ProyectoState>
   );
 }
---
+++ src/context/tareas/tareaContext.js
@@ -0,0 +1,5 @@
+import { createContext }from 'react'
+
+const TareaContext = createContex();
+
+export default TareaContext;
